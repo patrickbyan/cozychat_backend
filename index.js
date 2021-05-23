13454733707e3adf180fe0dcaeeb25fb16084980
patrickbyan <patrickbyann@gmail.com>
2021-05-23 13:21:35 +0700

    Fixed Throw Error: Couldn't Connect to Server

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors())
 const httpApp = http.createServer(app)
 const io = socket(httpApp)
 
+// Initialize DB on FREESQLHOSTING
 const PORT = process.env.PORT || 5000
 
 const db = mysql.createConnection({
@@ -20,113 +21,132 @@ const db = mysql.createConnection({
     port: 3306
 })
 
+// Initialize DB on Local Machine
+// const PORT = 5000
+
+// const db = mysql.createConnection({
+//     user: 'root', 
+//     password: 'purwadhika',
+//     database: 'cozychat',
+//     port: 3306
+// })
+
+
 app.get('/', (req, res) => {
     res.send('Welcome to Live Chat System API')
 })
 
+
+
 let userConnected = []
 
 io.on('connection', (socket) => {
-    console.log('User Connect With Id ' + socket.id)
-
-    socket.on('user-join', ({name, room}) => {
-        let userInRoom = userConnected.filter((value) => value.room === room)
-
-        if(userInRoom.length >= 5){
-            return socket.emit('total-user', userInRoom.length)
-        }else{
-            socket.emit('total-user', userInRoom.length)
-        }
-        
-        userConnected.push({
-            id: socket.id,
-            name: name,
-            room: room
-        })
-
-        userInRoom = userConnected.filter((value) => value.room === room)
-        socket.join(room)
-
-        db.query('SELECT * FROM chat_history WHERE room = ?', room, (err, result) => {
-            try {
-                if(err) throw err
-
-                socket.emit('get-chat-history', result)
-                io.in(room).emit('user-online', userInRoom)
-                socket.to(room).emit('send-message-from-server', {from: 'Bot', message: name + ' Joined'})
-                
-            } catch (error) {
-                console.log(error)
-            }
-        })
-    })
+    try {
+        if(!socket.id) throw { message: 'Cannot Connect to Server' }
     
-    socket.on("send-chat", (data) => {
-        let index = null
+        console.log('User Connect With Id ' + socket.id)
 
-        userConnected.forEach((value, idx) => {
-            if(value.id === socket.id){
-                index = idx
+        socket.on('user-join', ({name, room}) => {
+            let userInRoom = userConnected.filter((value) => value.room === room)
+
+            if(userInRoom.length >= 5){
+                return socket.emit('total-user', userInRoom.length)
+            }else{
+                socket.emit('total-user', userInRoom.length)
             }
-        })
+            
+            userConnected.push({
+                id: socket.id,
+                name: name,
+                room: room
+            })
 
-        let room = userConnected[index].room
+            userInRoom = userConnected.filter((value) => value.room === room)
+            socket.join(room)
 
-        let dataToInsert = {
-            socket_id: socket.id,
-            name: data.name,
-            room: room,
-            message: data.message
-        }
+            db.query('SELECT * FROM chat_history WHERE room = ?', room, (err, result) => {
+                try {
+                    if(err) throw err
 
-        db.query('INSERT INTO chat_history SET ?', dataToInsert, (err, result) => {
-            try {
-                if(err) throw err
-
-                socket.to(room).emit('send-chat-back', {from: data.name, message: data.message})
-                socket.emit('send-chat-back', {from: data.name, message: data.message})
-            } catch (error) {
-                console.log(error)
-            }
+                    socket.emit('get-chat-history', {error: false, result: result})
+                    io.in(room).emit('user-online', userInRoom)
+                    socket.to(room).emit('send-message-from-server', {from: 'Bot', message: name + ' Joined'})
+                    
+                } catch (error) {
+                    socket.emit('error-message', {error: true, message: 'Could Not Connect to Server', detail: error.sqlMessage})
+                }
+            })
         })
         
+        socket.on("send-chat", (data) => {
+            let index = null
+            console.log(data)
+            userConnected.forEach((value, idx) => {
+                if(value.id === socket.id){
+                    index = idx
+                }
+            })
 
-        
-    });
+            let room = userConnected[index].room
 
-    socket.on("typing-message", (data) => {
-        let index = null
-
-        userConnected.forEach((value, idx) => {
-            if(value.id === socket.id){
-                index = idx
+            let dataToInsert = {
+                socket_id: socket.id,
+                name: data.name,
+                room: room,
+                message: data.message
             }
+
+            db.query('INSERT INTO chat_history SET ?', dataToInsert, (err, result) => {
+                try {
+                    if(err) throw err
+
+                    socket.to(room).emit('send-chat-back', {from: data.name, message: data.message})
+                    socket.emit('send-chat-back', {from: data.name, message: data.message})
+                } catch (error) {
+                    socket.emit('error-message', {error: true, message: 'Could Not Connect to Server', detail: error.sqlMessage})
+                }
+            })
+        });
+
+        socket.on("typing-message", (data) => {
+            let index = null
+
+            userConnected.forEach((value, idx) => {
+                if(value.id === socket.id){
+                    index = idx
+                }
+            })
+
+            let room = userConnected[index].room
+
+            socket.to(room).emit('typing-message-back', {from: data.name, message: data.message})
         })
 
-        let room = userConnected[index].room
+        socket.on('disconnect', () => {
+            console.log('USER DISCONNECT')
 
-        socket.to(room).emit('typing-message-back', {from: data.name, message: data.message})
-    })
+            let index = null
 
-    socket.on('disconnect', () => {
-        console.log('USER DISCONNECT')
+            userConnected.forEach((value, idx) => {
+                if(value.id === socket.id){
+                    index = idx
+                }
+            })
 
-        let index = null
-
-        userConnected.forEach((value, idx) => {
-            if(value.id === socket.id){
-                index = idx
+            if(index !== null){
+                var name = userConnected[index].name
+                var room = userConnected[index].room
+                userConnected.splice(index, 1)
             }
+
+            socket.to(room).emit('send-message-from-server', {from: 'Bot', message: name + ' Left'})
         })
-
-        if(index !== null){
-            var name = userConnected[index].name
-            var room = userConnected[index].room
-            userConnected.splice(index, 1)
-        }
-
-        socket.to(room).emit('send-message-from-server', {from: 'Bot', message: name + ' Left'})
-    })
+    } catch (error) {
+        res.status(500).send({
+            error: true,
+            message: error.message
+        })
+    }
 })
 
 httpApp.listen(PORT, () => {
