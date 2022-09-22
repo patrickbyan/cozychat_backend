3c53277222a1a67a96f039d9ad6e7ae7d9e61867
Patrick Byan Katamsi <77653156+patrickbyan@users.noreply.github.com>
2022-09-22 11:10:02 +0700

    refactor

--- index.js
+++ index.js
@@ -11,25 +11,40 @@ const httpApp = http.createServer(app)
 const io = socket(httpApp)
 
 // Initialize DB on FREESQLHOSTING
+const {
+    PORT,
+    env,
+    DB_HOST,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_PORT,
+    LOCAL_DB_HOST,
+    LOCAL_DB_NAME,
+    LOCAL_DB_USER,
+    LOCAL_DB_PASSWORD,
+    LOCAL_DB_PORT
+} = process.env
+
 const PORT = process.env.PORT || 5000
 
-const db = mysql.createConnection({
-    host: 'sql6.freemysqlhosting.net',
-    database: 'sql6409077',
-    user: 'sql6409077', 
-    password: '35H1hqcj4F',
-    port: 3306
-})
+const sqlProps = {
+    host: DB_HOST,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    PORT: DB_PORT
+}
 
-// Initialize DB on Local Machine
-// const PORT = 5000
+if (env === 'production') {
+    sqlProps.host = LOCAL_DB_HOST
+    sqlProps.database = LOCAL_DB_NAME
+    sqlProps.user = LOCAL_DB_USER
+    sqlProps.password = LOCAL_DB_PASSWORD
+    sqlProps.port = LOCAL_DB_PORT
+}
 
-// const db = mysql.createConnection({
-//     user: 'root', 
-//     password: 'purwadhika',
-//     database: 'cozychat',
-//     port: 3306
-// })
+const db = mysql.createConnection(sqlProps)
 
 
 app.get('/', (req, res) => {
@@ -37,24 +52,23 @@ app.get('/', (req, res) => {
 })
 
 
-
 let userConnected = []
 
 io.on('connection', (socket) => {
     try {
-        if(!socket.id) throw { message: 'Cannot Connect to Server' }
-    
+        if (!socket.id) throw { message: 'Cannot Connect to Server' }
+
         console.log('User Connect With Id ' + socket.id)
 
-        socket.on('user-join', ({name, room}) => {
+        socket.on('user-join', ({ name, room }) => {
             let userInRoom = userConnected.filter((value) => value.room === room)
 
-            if(userInRoom.length >= 5){
-                return socket.emit('total-user', userInRoom.length)
-            }else{
-                socket.emit('total-user', userInRoom.length)
+            socket.emit('total-user', userInRoom.length)
+
+            if (userInRoom.length >= 5) {
+                return 
             }
-            
+
             userConnected.push({
                 id: socket.id,
                 name: name,
@@ -66,23 +80,23 @@ io.on('connection', (socket) => {
 
             db.query('SELECT * FROM chat_history WHERE room = ?', room, (err, result) => {
                 try {
-                    if(err) throw err
+                    if (err) throw err
 
-                    socket.emit('get-chat-history', {error: false, result: result})
+                    socket.emit('get-chat-history', { error: false, result: result })
                     io.in(room).emit('user-online', userInRoom)
-                    socket.to(room).emit('send-message-from-server', {from: 'Bot', message: name + ' Joined'})
-                    
+                    socket.to(room).emit('send-message-from-server', { from: 'Bot', message: name + ' Joined' })
+
                 } catch (error) {
-                    socket.emit('error-message', {error: true, message: 'Could Not Connect to Server', detail: error.sqlMessage})
+                    socket.emit('error-message', { error: true, message: 'Could Not Connect to Server', detail: error.sqlMessage })
                 }
             })
         })
-        
+
         socket.on("send-chat", (data) => {
             let index = null
-            console.log(data)
+
             userConnected.forEach((value, idx) => {
-                if(value.id === socket.id){
+                if (value.id === socket.id) {
                     index = idx
                 }
             })
@@ -98,12 +112,12 @@ io.on('connection', (socket) => {
 
             db.query('INSERT INTO chat_history SET ?', dataToInsert, (err, result) => {
                 try {
-                    if(err) throw err
+                    if (err) throw err
 
-                    socket.to(room).emit('send-chat-back', {from: data.name, message: data.message})
-                    socket.emit('send-chat-back', {from: data.name, message: data.message})
+                    socket.to(room).emit('send-chat-back', { from: data.name, message: data.message })
+                    socket.emit('send-chat-back', { from: data.name, message: data.message })
                 } catch (error) {
-                    socket.emit('error-message', {error: true, message: 'Could Not Connect to Server', detail: error.sqlMessage})
+                    socket.emit('error-message', { error: true, message: 'Could Not Connect to Server', detail: error.sqlMessage })
                 }
             })
         });
@@ -112,14 +126,14 @@ io.on('connection', (socket) => {
             let index = null
 
             userConnected.forEach((value, idx) => {
-                if(value.id === socket.id){
+                if (value.id === socket.id) {
                     index = idx
                 }
             })
 
             let room = userConnected[index].room
 
-            socket.to(room).emit('typing-message-back', {from: data.name, message: data.message})
+            socket.to(room).emit('typing-message-back', { from: data.name, message: data.message })
         })
 
         socket.on('disconnect', () => {
@@ -128,18 +142,18 @@ io.on('connection', (socket) => {
             let index = null
 
             userConnected.forEach((value, idx) => {
-                if(value.id === socket.id){
+                if (value.id === socket.id) {
                     index = idx
                 }
             })
 
-            if(index !== null){
+            if (index !== null) {
                 var name = userConnected[index].name
                 var room = userConnected[index].room
                 userConnected.splice(index, 1)
             }
 
-            socket.to(room).emit('send-message-from-server', {from: 'Bot', message: name + ' Left'})
+            socket.to(room).emit('send-message-from-server', { from: 'Bot', message: name + ' Left' })
         })
     } catch (error) {
         res.status(500).send({
